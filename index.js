6ac27a62313d91c219e63b00e3f0717527f20468
dnunez10 <diana_nunez10@yahoo.com>
2021-02-28 14:36:50 -0800

    commit tree d4f86a1b7b235216bd4e311ec0be1a510c5e33d2

---
+++ index.js
@@ -0,0 +1,19 @@
+receivesAFunction = (callback) => {
+    callback();
+}
+receivesAFunction(spy);
+
+const createSpy = () => {
+}
+
+before(function () {
+    returnsANamedFunction = (fn) => {
+    return fn;
+    }
+})
+
+before(function () {
+    returnsAnAnonymousFunction = () => {
+        return fn;
+    }
+})
---
+++ test.js
@@ -0,0 +1,7 @@
+function copyArrayAndMultiplyBy2(array) {
+    let output = [];
+    for (let i = 0; i < array.length; i++) {
+        output.push(array[i] * 2);
+    }
+    console.log(copyArrayAndMultiplyBy2);
+}
